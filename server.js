6d457004f8119d5ba075a02c145a380d614738a8
umarbinmusa <umarmusa20002@gmail.com>
2026-08-19 00:18:06 +0100

    update

--- server.js
+++ server.js
@@ -36,12 +36,15 @@ const startServer = async () => {
     })
   );
 
-  app.use(
-    cors({
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://app.assalamtelecom.com.ng"
+  ],
+  credentials: true
+}));
 
   app.use(express.json({ limit: "5mb" }));
 
